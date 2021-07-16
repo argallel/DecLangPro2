d92d60ebda5dd542c1e87b3fbfd6f276667cf2c7
Katherine Argall <katherine@argall.ca>
2021-07-16 09:24:48 -0400

    Initial commit

---
+++ Project2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Program:         Project2.exe
+ * Course:          INFO-3138
+ * Date:            July 30, 2021
+ * Author:          K. Argall, R. Turner
+ * Description:     A C# console program that uses the DOM to display information from
+ *                  the file ghg-canada.xml and allows to user to choose what to display
+ */
+
+
+using System;
+using System.Xml;
+
+namespace Project2
+{
+    class Program
+    {
+        const string XML_FILE = "ghg-canada.xml";
+        static void Main(string[] args)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(XML_FILE);
+            }
+            catch (XmlException ex)
+            {
+                Console.WriteLine($"DOM ERROR: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"GENERAL ERROR: {ex.Message}");
+            }
+        }
+    }
+}
